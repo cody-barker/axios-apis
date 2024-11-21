6c2592308a04cbbb3bab9fa70905e056b5faad0b
Cody <codybarker.or@gmail.com>
2024-11-21 10:19:11 -0800

    update baseURL with https prefix

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "www.portlandnativeplants.com",
+  baseURL: "https://www.portlandnativeplants.com",
   timeout: 5000,
   headers: {
     "Content-Type": "application/json",
