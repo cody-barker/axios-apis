a6c845eb820fc1c2b00c8825b9a8b41996f4e025
Cody <codybarker.or@gmail.com>
2024-11-21 10:27:40 -0800

    update usersApi to handle array of errors from API, set errors state to empty array in UsersPage

--- src/api/usersApi.js
+++ src/api/usersApi.js
@@ -1,9 +1,19 @@
 import axiosInstance from "./axiosInstance";
 
-//get users
+// Get all users
 export const getUsers = async () => {
-  const response = await axiosInstance.get("/users");
-  return response.data;
+  try {
+    const response = await axiosInstance.get("/users");
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data.errors) {
+      // Assuming API returns an array of errors in response to a GET request
+      throw error.response.data.errors;
+    } else {
+      // Generic error if no validation errors are present
+      throw [{ message: "An error occurred while fetching users" }];
+    }
+  }
 };
 
 //get users by id
@@ -12,10 +22,21 @@ export const getUserById = async (id) => {
   return response.data;
 };
 
-//post user with userData
+// Example API call with error handling
 export const createUser = async (userData) => {
-  const response = await axiosInstance.post("/users", userData);
-  return response.data;
+  try {
+    const response = await axiosInstance.post("/users", userData);
+    return response.data; // If successful, return the user data
+  } catch (error) {
+    // Check if the error response contains validation errors
+    if (error.response && error.response.data.errors) {
+      // Assuming the API returns an array of errors
+      throw error.response.data.errors;
+    } else {
+      // Generic error if validation errors aren't present
+      throw [{ message: "An error occurred while creating the user" }];
+    }
+  }
 };
 
 //update user by id with userData
