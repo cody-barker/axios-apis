c4a515cc59987c1f6af64d6c029ddc8b665c9311
Cody <codybarker.or@gmail.com>
2024-11-21 10:10:00 -0800

    add usersApi data

---
+++ src/api/usersApi.js
@@ -0,0 +1,31 @@
+import axiosInstance from "./axiosInstance";
+
+//get users
+export const getUsers = async () => {
+  const response = await axiosInstance.get("/users");
+  return response.data;
+};
+
+//get users by id
+export const getUserById = async () => {
+  const response = await axiosInstance.get(`/users/${id}`);
+  return response.data;
+};
+
+//post user with userData
+export const createUser = async (userData) => {
+  const response = await axiosInstance.post("/users", userData);
+  return response.data;
+};
+
+//update user by id with userData
+export const updateUser = async (id, userData) => {
+  const response = await axiosInstance.patch(`/users/${id}`, userData);
+  return response.data;
+};
+
+//delete user by id
+export const deleteUser = async (id) => {
+  const response = await axiosInstance.delete(`/users/${id}`);
+  return response.data;
+};
